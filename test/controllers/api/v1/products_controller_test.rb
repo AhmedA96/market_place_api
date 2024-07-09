3147516139db98ac2d9d16fe4076f5bca9b9e8a4
Ahmed Ali <ahmed.ga1996@gmail.com>
2024-07-10 00:11:39 +0300

    Finishes modeling the product model along with the user associations

---
+++ test/controllers/api/v1/products_controller_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Api
+  module V1
+    class ProductsControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        @product = products(:one)
+      end
+
+
+      test 'should show products' do
+        get api_v1_products_url(), as: :json
+        assert_response :success
+      end
+
+      test 'should show product' do
+        get api_v1_product_url(@product), as: :json
+        assert_response :success
+
+        json_response = JSON.parse(response.body)
+        assert_equal @product.title, json_response['title']
+      end
+    end
+  end
+end
